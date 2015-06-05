12cdbdab168d12e590cbc4c03cf517ba255bae10
Narendran Thangarajan <narendran.thangarajan@gmail.com>
2015-06-05 07:03:38 -0700

    First ever code and plot for blog! Woohoo

---
+++ mongo-vs-rethink/mongo_write_no_index.py
@@ -0,0 +1,27 @@
+import pymongo
+import time
+
+client = pymongo.MongoClient()
+db = client.test_db
+coll = db.test_collection
+
+def get_time():
+	return time.time() * 1000 * 1000
+
+# Drop collection if exists. Create a single document to create the required files 
+# because we want to capture real-world usage scenario.
+coll.drop()
+doc = {"a" : 1, "b" : "name", "c" : True}
+coll.insert(doc)
+
+i = 0
+while i < 1000:
+	# Reinitializing doc to avoid _id index conflict. pymongo stores the persisted document in "doc"
+	doc = {"a" : 1, "b" : "name", "c" : True}
+	start = get_time()
+	coll.insert(doc)
+	time_taken = get_time() - start
+	print time_taken
+	i += 1
+
+client.close()
---
+++ mongo-vs-rethink/rethinkdb_write_noindex.py
@@ -0,0 +1,24 @@
+import rethinkdb as r
+import time
+
+def get_time():
+	return time.time() * 1000 * 1000
+
+connection = r.connect( "localhost", 28015)
+try:
+	r.db_drop("test_db").run(connection)
+	r.db("test_db").table_drop("test_table").run(connection)
+except r.RqlRuntimeError, e:
+	pass # Ignoring non-existent table drop
+
+r.db_create("test_db").run(connection)
+r.db("test_db").table_create("test_table").run(connection)
+r.db("test_db").table("test_table").insert([{"a" : 1, "b" : "name", "c" : True}]).run(connection)
+
+i = 0
+while i < 1000:
+	start = get_time()
+	r.db("test_db").table("test_table").insert([{"a" : 1, "b" : "name", "c" : True}]).run(connection)
+	time_taken = get_time() - start
+	print time_taken
+	i += 1
